fda9cba4496ef5d8bece892c4e90725c1f1dada2
Urmik Godhani <urmikgodhani000@gmail.com>
2022-07-17 14:00:13 +0530

    production build

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,24 @@ app.use("/api/user", userRouters);
 app.use("/api/chat", chatRouters);
 app.use("/api/message", messageRouters);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 app.use(notFound);
 app.use(errorHandler);
 
